991c8f4116a06b259fc556e73f491ce9244aa897
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-11-30 21:50:29 -0300

    get pokemon details

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,19 @@
 const pokeapi = {}
 
+pokeapi.getPokemonDetails = (pokemon) => {
+    return  fetch(pokemon.url)
+                .then(response => response.json())
+}
+
 pokeapi.getPokemons = (offset=0, limit=10) => {
 const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`
 
 return fetch(url)
     .then(response => response.json())
-    .then(data => data.results)
+    .then(jsonBody => jsonBody.results)
+    .then(pokemons => Promise.all(pokemons.map(pokeapi.getPokemonDetails)))
+    .then(pokemonsDetails => pokemonsDetails)
     .catch(error => console.error('Error:', error))
+
 }
 
