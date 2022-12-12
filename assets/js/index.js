3fe0db07e5acf31d4a6a75543cb15304f4e251c0
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-12-12 18:21:46 -0300

    convert pokemon to li.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,17 @@
-function convertsPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map(typeSlot => `<li class="type">${typeSlot.type.name}</li>`).join('')
-}
-
-function getPokemonImg(pokemon) {
-    return 
-}
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
 function pokemonToLi(pokemon){
-    return `<li class="pokemon">
-    <span class="number">#${pokemon.order}</span>
+    return `<li class="pokemon ${pokemon.type}">
+    <span class="number">#${pokemon.number}</span>
     <span class="name">${capitalizeFirstLetter(pokemon.name)}</span>
 
     <div class="detail">
         <ol class="types">
-            ${convertsPokemonTypesToLi(pokemon.types)}
+            ${pokemon.types.map(type => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
         </ol>
-        <img src=${pokemon.sprites.other.dream_world.front_default} alt="${pokemon.name}">
+        <img src=${pokemon.photo} alt="${pokemon.name}">
     </div>
 </li>`
 }
