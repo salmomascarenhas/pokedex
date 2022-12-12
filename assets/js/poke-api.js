715c550bc93f462669690e745cf0bf3b0be69dad
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-12-12 18:21:27 -0300

    create model for pokemon.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,26 @@
 const pokeapi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map(typeSlot => typeSlot.type.name)
+    const [type] = types
+
+
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeapi.getPokemonDetails = (pokemon) => {
     return  fetch(pokemon.url)
                 .then(response => response.json())
+                .then(convertPokeApiDetailToPokemon)
 }
 
 pokeapi.getPokemons = (offset=0, limit=10) => {
@@ -14,6 +32,5 @@ return fetch(url)
     .then(pokemons => Promise.all(pokemons.map(pokeapi.getPokemonDetails)))
     .then(pokemonsDetails => pokemonsDetails)
     .catch(error => console.error('Error:', error))
-
 }
 
